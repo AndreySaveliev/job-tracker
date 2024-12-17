086448e2d4b55e2c8538aacf9a5d715287a1fe25
Andrey <viiixx@yandex.ru>
2024-12-17 16:10:14 +0300

    session validation
    if user already logged in home page can be visited. else user no logged in redirect to /signin page

---
+++ session/dal.ts
@@ -0,0 +1,15 @@
+import "server-only";
+
+import { cookies } from "next/headers";
+import { decrypt } from "./session";
+import { cache } from "react";
+import { redirect } from "next/navigation";
+
+export const verifySession = async () => {
+  const cookie = (await cookies()).get("session")?.value;
+  const session = await decrypt(cookie);
+  if (!session?.userId) {
+    redirect("/signup");
+  }
+  return { isAuth: true, userId: session?.userId as string };
+};
